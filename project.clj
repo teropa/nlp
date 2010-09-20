0d79f9b89784b5b877cfe48dcc03df05ee6f70d0
Tero Parviainen <tero.parviainen@eficode.fi>
2010-09-20 10:02:09 +0300

    converted pickled punkt parser params

--- project.clj
+++ project.clj
@@ -1,3 +1,5 @@
 (defproject teropa/nlp "1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]])
+                 [org.clojure/clojure-contrib "1.2.0"]
+                 [commons-io "1.4"]])
+
---
+++ src/teropa/nlp/util/conversion/punkt_tokenizer_conversion.clj
@@ -0,0 +1,47 @@
+(ns teropa.nlp.util.conversion.punkt-tokenizer-conversion
+  "Converts pretrained punkt tokenizers from Python pickle to serialized
+   Clojure data structures"
+  (:require [clojure.java.shell :as sh])
+  (:require [clojure.contrib.json :as json])
+  (:require [clojure.contrib.duck-streams :as streams])
+  (:require [clojure.pprint :as pp])
+  (:import [java.io File])
+  (:import [org.apache.commons.io FilenameUtils FileUtils]))
+
+(defn python-command [f]
+  (str "import nltk.data\n"
+       "import json\n"
+       "e = nltk.data.LazyLoader('tokenizers/punkt/" (.getName f) "')\n"
+       "print json.dumps({'ortho-context': e._params.ortho_context, 'abbrev_types': list(e._params.abbrev_types), 'collocations': list(e._params.collocations)})\n"))
+
+(defn- data-path []
+  (.getAbsolutePath (File. "resources")))
+
+(defn- clojure-file [f]
+  (let [res (File.
+              (str (FilenameUtils/getFullPath (.getAbsolutePath f))
+                   (FilenameUtils/getBaseName (.getAbsolutePath f))
+                   ".clj"))]
+    (if-not (.exists res)
+      (.createNewFile res))
+    res))
+
+(defn run []
+  (doseq [f (FileUtils/listFiles
+              (File. "resources/tokenizers/punkt")
+              (into-array ["pickle"])
+              false)]
+    (let [res (sh/sh "python" "-c" (python-command f)
+                :env {"NLTK_DATA" (data-path)})
+          params (json/read-json (:out res) false)]
+      (with-open [w (streams/writer (clojure-file f))]
+        (binding [*out* w]
+          (pp/pprint {:ortho-context (params "ortho-context")
+                      :abbrev-types (params "abbrev_types")
+                      :collocations (params "collocations")}))))))
+
+
+
+
+
+
